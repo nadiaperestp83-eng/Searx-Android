8b3a43b00b0aa43c96994a1b408d07771bf8ba55
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-23 19:44:29 -0300

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,53 +1,32 @@
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-String? searxURL;
-String defaultURL = "https://searx.fmac.xyz";
-String title = 'Searx';
+
+const String kDefaultURL = 'https://searxng-railway-production-9bcc.up.railway.app';
+const String kDefaultSafeSearch = '0'; // 0=Nada, 1=Moderada, 2=Rigorosa
 
 class Settings {
-  Future<SharedPreferences> get prefs async =>
+  static const _keyURL = 'searxng_url';
+  static const _keySafeSearch = 'safe_search';
+
+  Future<SharedPreferences> get _prefs async =>
       await SharedPreferences.getInstance();
 
-  void setURL(String data) async {
-    await (await prefs).setString("url", data);
+  Future<String> getURL() async {
+    final prefs = await _prefs;
+    return prefs.getString(_keyURL) ?? kDefaultURL;
   }
 
-  Future<void> errURL(url) async {
-    String msg;
-    if (url == defaultURL) {
-      msg = "Unable to load default instance! Change instance to something else";
-      title = 'Searx';
-    } else {
-      msg = "Invalid Searx URL! Setting to default";
-      searxURL = defaultURL;
-      await (await prefs).remove("url");
-    }
-    await Fluttertoast.showToast(
-      msg: msg,
-      toastLength: Toast.LENGTH_SHORT,
-      timeInSecForIosWeb: 2,
-      backgroundColor: Colors.red,
-      textColor: Colors.white,
-      fontSize: 16.0
-    );
+  Future<void> setURL(String url) async {
+    final prefs = await _prefs;
+    await prefs.setString(_keyURL, url);
   }
 
-  Future<String?> getURL() async {
-    searxURL = (await prefs).getString("url") ?? defaultURL;
-    try {
-      var response = (await http.get(Uri.parse(searxURL!))).statusCode;
-      if (response != 200) {
-        await errURL(searxURL);
-        }
-      } catch (err) {
-        await errURL(searxURL);
-      }
-    // Set title of appbar to title of searx instance (webpage title)
-    var webpage = (await http.read(Uri.parse(searxURL!)));
-    title = webpage.substring((webpage.indexOf('<title>') + 7), (webpage.indexOf('</title>')));
-    // Have to return something for buildMain to trigger
-    return searxURL;
+  Future<String> getSafeSearch() async {
+    final prefs = await _prefs;
+    return prefs.getString(_keySafeSearch) ?? kDefaultSafeSearch;
+  }
+
+  Future<void> setSafeSearch(String value) async {
+    final prefs = await _prefs;
+    await prefs.setString(_keySafeSearch, value);
   }
 }
