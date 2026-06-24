feb0ac73fd778ac2f209311f4606bcdd62f66ac0
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-23 21:35:40 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/searx/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/searx/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.zackptg5.searx
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.platformViewsController.registry.registerViewFactory(
+            "searxgo/private_browser",
+            PrivateBrowserFactory(flutterEngine.dartExecutor.binaryMessenger)
+        )
+    }
 }
